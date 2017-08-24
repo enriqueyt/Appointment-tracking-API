4e69944474eb7359db1ed42cd6c398bac58d6ab3
Enriqueyt <enriqueyt@gmail.com>
2017-08-24 15:31:19 -0500

    added router to get dashborads

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var ObjectId = require('mongoose').Types.ObjectId;
+var user = mongoose.model('user');
+var appointment = mongoose.model('appointment');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,7 +11,36 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/dashboard/:id', function(req, res){
-  var _id = rew.params.id;
+  var _id = req.params.id;
+  var query = {'_id':new ObjectId(_id)}
+
+  user.findById(query).exec()
+  .then(function(data, err){
+    if(err){
+      res.json({success:false, data:err})
+      return;
+    }    
+    query={}
+    if(data.role[0].toLowerCase()=='Manage'){
+      query={};
+    }
+    return appointment.find(query).exec();
+  })
+  .then(function(data){
+      var obj={
+          citasEfectivas:0,          
+          citasPorAtender:0,
+          citasAtendidas:0,
+          totalCitas:data.length
+      },
+      objaux;
+      for(var i=0; i<data.length;i++){
+        objaux=data[i];
+        if(objaux.wasAttended&&objaux.products.length>0) obj.citasEfectivas++;
+        if(objaux.wasAttended) obj.citasAtendidas++;
+      };
+      res.json({success:true, data:obj})
+  })
 });
 
 module.exports = router;
